c9af7cdf0fdd5a76b4547a5db76fb28c81172c36
Aaron J Gerbert <aaronred345@gmail.com>
2025-02-03 20:54:39 -0500

    Using Nuitka to create native application
    Don't need to install nuitka yourself, although it is a useful tool.
    The compile.sh script will create a python virtual environment, install nuitka in the venv. After creating the executable, it will delete the virtual environment
    Also added cleanup script to remove files created from compiling

--- install-global.sh
+++ install-global.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-python3 -m compileall ./
-mkdir ./build
-mv ./__pycache__/* ./build/steam-prefix
-chmod +x ./build/steam-prefix
-rm -rf ./__pycache__/
-sudo cp ./build/steam-prefix /bin/steam-prefix
+source ./compile.sh
+sudo cp ./main.bin /bin/steam-prefix
 echo "/bin/steam-prefix > /dev/null 2>&1 &" > ./steam-prefix.sh
 sudo mv ./steam-prefix.sh /etc/profile.d/steam-prefix.sh
+source ./cleanup.sh
--- install-local.sh
+++ install-local.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-python3 -m compileall ./
-mkdir ./build
-mv ./__pycache__/* ./build/steam-prefix
-chmod  +x ./build/steam-prefix
-rm -rf ./__pycache__/
-cp ./build/steam-prefix $HOME/.local/bin/steam-prefix
+source ./compile.sh
+cp ./main.bin $HOME/.local/bin/steam-prefix
 echo "$HOME/.local/bin/steam-prefix > /dev/null 2>&1 &" >> $HOME/.profile
+source ./cleanup.sh
